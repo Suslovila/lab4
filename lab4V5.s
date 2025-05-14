f1e2c1cd73b6e0898d904c357e10b8ab9e9922ce
suslov.im <102728898+Suslovila@users.noreply.github.com>
2025-05-14 23:12:58 +0300

    rgrg

--- lab4V5.s
+++ lab4V5.s
@@ -1,177 +1,164 @@
 global main
-extern printf, scanf, fopen, fprintf, fclose, fabs
+extern printf, scanf, fopen, fprintf, fclose
+extern pow, log, fabs
 
 section .data
-    fmt_scan:   db "%f",0
-    fmt_sum:    db "cos^2(x) ≈ %f",10,0
-    fmt_file:   db "n = %d term = %f",10,0
-    fmt_errarg: db "Usage: %s <terms_file>",10,0
-    mode_w:     db "w",0
-
-    one:        dq 1.0
-    four:       dq 4.0
-    neg_one:    dq -1.0
+  fmt_scan:   db      "%f",0
+;   fmt_pow:    db      "pow(a, x) = %f", 10,0
+  fmt_sum:    db      "series sum = %f", 10,0
+  fmt_file:   db      "n = %d term = %f", 10,0
+  fmt_errarg: db      "Usage: %s <terms_file>", 10,0
+  fmt_erra:   db      "Error: a must be > 0", 10,0
+  mode_w:     db      "w",0
+  one:        dq      1.0
 
 section .bss
-    x:          resd 1
-    eps:        resd 1
-    sum_res:    resq 1
-    term_cur:   resq 1
-    x2_val:     resq 1
-    fp:         resq 1
+  a:          resd    1
+  x:          resd    1
+  eps:        resd    1
+  pow_res:    resq    1    ;для pow(a, x) 
+  sum_res:    resq    1    ;для накопления суммы
+  term_cur:   resq    1    ;текущий член ряда
+  t_val:      resq    1    ;для x * ln(a)
+  fp:         resq    1    
 
 section .text
 main:
-    push    rbp
-    mov     rbp, rsp
-
-    ; argv check
-    cmp     rdi, 2
-    jge     have_file
+  push    rbp
+    mov     rbp,rsp
+    
+    cmp     rdi, 2      ;проверка количества аргументов
+    jge     .have_file
+     ;если меньше 2 ошибка и возвращаем 1
     mov     rdi, fmt_errarg
-    mov     rsi, [rsp+16]
+    mov     rsi, [rsp + 16]
     xor     eax, eax
     call    printf
     mov     eax, 1
-    jmp     exit
+    jmp     .exit
 
-have_file:
-    ; filename = argv[1]
-    mov     rbx, rsi
-    mov     r12, [rbx+8]
+.have_file:
 
-    ; read x
+  mov     rbx, rsi        
+    mov     r12, [rbx + 8]    ;в r12 имя исходного файла
+    ;ввод a
+    mov     rdi, fmt_scan
+    lea     rsi, [rel a]
+    xor     eax, eax
+    call    scanf
+    ;ввод x
     mov     rdi, fmt_scan
     lea     rsi, [rel x]
     xor     eax, eax
     call    scanf
-
-    ; read eps
+    ;ввод eps
     mov     rdi, fmt_scan
     lea     rsi, [rel eps]
     xor     eax, eax
     call    scanf
-
-    ; open file
-    mov     rdi, r12
+  ;проверка на a > 0
+  movss   xmm0, [rel a]
+    pxor    xmm1, xmm1
+    ucomiss xmm0, xmm1
+    jbe     .err_a_le0
+  ;pow_res = точное вычисление
+  movss    xmm0, [rel a]
+    cvtps2pd xmm0, xmm0       
+    movss    xmm1, [rel x]
+    cvtps2pd xmm1, xmm1        
+    call    pow               
+    movsd   [rel pow_res], xmm0
+  ;считаем ln(a)
+  movss    xmm0, [rel a]
+    cvtps2pd xmm0, xmm0
+    call     log
+    movsd    xmm1, xmm0    
+    ;считаем x * ln(a)      
+    movss    xmm0, [rel x]
+    cvtps2pd xmm0, xmm0         
+    mulsd    xmm0, xmm1     
+    movsd    [rel t_val], xmm0
+  ;начальные значения term = sum = 1.0
+    movsd   xmm0, [rel one]
+  movsd   [rel term_cur], xmm0
+    movsd   [rel sum_res], xmm0
+  ;открываем файл для записи членов ряда
+  mov     rdi, r12          
     mov     rsi, mode_w
     call    fopen
     mov     [rel fp], rax
     test    rax, rax
-    je      err_fopen
-
-    ; x → double, x2 = x*x
-    movss   xmm0, [rel x]
-    cvtps2pd xmm0, xmm0
-    movapd  xmm1, xmm0
-    mulsd   xmm1, xmm1
-    movsd   [rel x2_val], xmm1
-
-    ; sum_res = 1.0
-    movsd   xmm0, [rel one]
-    movsd   [rel sum_res], xmm0
-
-    xor     r14d, r14d
-
-    ; n=1, term = –x^2
-    mov     r14d, 1
-    movsd   xmm0, [rel x2_val]
-    mulsd   xmm0, [rel neg_one]
-    movsd   [rel term_cur], xmm0
-
-    ; if |term| < eps → done
-    movsd   xmm0, [rel term_cur]
-    call    fabs
-    cvtsd2ss xmm2, xmm0
-    movss   xmm3, [rel eps]
-    ucomiss xmm2, xmm3
-    jb      done_series
-
-    ; sum += term
+    je      .err_fopen
+    
+    xor     r14d, r14d  
+;цикл разложения
+.loop_series:
+  inc     r14d  
+  ;term *= t_val / n            
+    movsd   xmm0, [rel t_val]
+    cvtsi2sd xmm1, r14d
+    divsd   xmm0, xmm1         
     movsd   xmm1, [rel term_cur]
+    mulsd   xmm1, xmm0         
+    movsd   [rel term_cur], xmm1    
+  ;sum += term
     movsd   xmm0, [rel sum_res]
     addsd   xmm0, xmm1
     movsd   [rel sum_res], xmm0
-
-    ; fprintf first term
+    ;если term < eps конец
+    movsd   xmm0, xmm1
+    call    fabs
+    cvtsd2ss xmm1, xmm0       
+    movss   xmm2, [rel eps]
+    ucomiss xmm1, xmm2
+    jb      .done_series   
+  ;fprintf(...)
     mov     rdi, [rel fp]
     mov     rsi, fmt_file
-    mov     rdx, r14
+    mov     edx, r14d
     cvtsd2ss xmm0, [rel term_cur]
     cvtss2sd xmm0, xmm0
-    mov     eax, 1
+    mov     eax, 1             
     call    fprintf
-
-.loop_series:
-    inc     r14d
-
-    ; factor = 4*x^2
-    movsd   xmm0, [rel x2_val]
-    mulsd   xmm0, [rel four]
-
-    ; denom = 2n*(2n-1)
-    lea     edx, [r14 + r14]    ; edx = 2*n
-    mov     ecx, edx
-    dec     ecx                 ; ecx = 2n-1
-    imul    edx, ecx            ; edx = 2n*(2n-1)
-
-    ; factor /= denom
-    cvtsi2sd xmm1, edx
-    divsd   xmm0, xmm1
-
-    ; term *= factor, change sign
-    movsd   xmm2, [rel term_cur]
-    mulsd   xmm2, xmm0
-    mulsd   xmm2, [rel neg_one]
-    movsd   [rel term_cur], xmm2
-
-    ; if |term| < eps → done
-    movsd   xmm0, xmm2
-    call    fabs
-    cvtsd2ss xmm3, xmm0
-    movss   xmm4, [rel eps]
-    ucomiss xmm3, xmm4
-    jb      done_series
-
-    ; sum += term
-    movsd   xmm1, xmm2
-    movsd   xmm0, [rel sum_res]
-    addsd   xmm0, xmm1
-    movsd   [rel sum_res], xmm0
-
-    ; fprintf term
-    mov     rdi, [rel fp]
-    mov     rsi, fmt_file
-    mov     rdx, r14
-    cvtsd2ss xmm0, xmm2
-    cvtss2sd xmm0, xmm0
-    mov     eax, 1
-    call    fprintf
-
     jmp     .loop_series
-
-done_series:
-    ; close file
-    mov     rdi, [rel fp]
+;конец цикла и печать результата
+.done_series:
+  mov     rdi, [rel fp]
     call    fclose
+  ;printf(pow...)
 
-    ; print result
+    ; mov     rdi, fmt_pow
+    ; cvtsd2ss xmm0, [rel pow_res]
+    ; cvtss2sd xmm0, xmm0
+    ; mov     eax, 1
+    ; call    printf
+
+
+    ;printf(series sum...)
     mov     rdi, fmt_sum
-    movsd   xmm0, [rel sum_res]
-    cvtsd2ss xmm0, xmm0
+    cvtsd2ss xmm0, [rel sum_res]
     cvtss2sd xmm0, xmm0
-    xor     eax, eax
+    mov     eax, 1
     call    printf
 
-exit:
-    mov     rsp, rbp
-    pop     rbp
-    ret
-
-err_fopen:
-    mov     rdi, fmt_errarg
-    mov     rsi, [rsp+16]
+    xor     eax, eax
+    
+.exit:
+  mov rsp, rbp
+  pop rbp
+  ret
+;a < 0
+.err_a_le0:
+  mov     rdi, fmt_erra
     xor     eax, eax
     call    printf
     mov     eax, 1
-    jmp     exit
+    jmp     .exit
+;файл не открылся
+.err_fopen:
+  mov     rdi, fmt_errarg
+  mov     rsi, [rsp+16]
+  xor     eax, eax
+    call    printf
+  mov     eax, 1
+  jmp     .exit
